292a4ee8f30a55a1e63d3eb957c151f4de52128d
Darren <darrenellis77@gmail.com>
2017-01-11 17:29:31 -0700

    Add files via upload

---
+++ config/application.rb
@@ -0,0 +1,27 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module QbDev
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    
+    # -- all .rb files in that directory are automatically loaded.
+  ActionMailer::Base.smtp_settings = {
+
+  	:address	=> 'smtp.gmail.com',
+  	:domain		=> 'mail.google.com',
+  	:prot 		=> 587,
+  	:user_name 	=> "quikbyteapp@gmail.com", #ENV['GMAIL_USERNAME'],
+  	:password	=> "smartfood", #ENV ['GMAIL_PASSWORD'],
+    :authentication => 'login',
+  	:enable_starttls_auto => true
+  }
+  end
+end
+
