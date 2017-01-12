21ac310b6703392926b8f550a5bf579bbd46a38e
Darren <darrenellis77@gmail.com>
2017-01-11 18:24:58 -0700

    Add files via upload

---
+++ test/controllers/account_activations_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class AccountActivationsControllerTest < ActionDispatch::IntegrationTest
+  test "should get \" do
+    get account_activations_\_url
+    assert_response :success
+  end
+
+end
