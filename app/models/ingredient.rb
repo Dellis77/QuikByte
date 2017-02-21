9e9ccbcd00225715a787b5ed8326c3ec717f4ae8
Luis Menchaca <cradelborgir@gmail.com>
2017-02-21 23:39:08 +0000

    spell fix

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,5 +2,5 @@ class Ingredient < ApplicationRecord
 	has_many :users_has_ingredients
 	has_many :users, through: :users_has_ingredients
 	has_many :recipe_has_ingredients
-	has_many :recpies, through: :recipe_has_ingredients
+	has_many :recipes, through: :recipe_has_ingredients
 end
