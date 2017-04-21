dc30a6fd8aa9e33c881443be8be7686eca85a85f
Luis Menchaca <cradelborgir@gmail.com>
2017-04-21 20:21:31 +0000

    new ing working

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -84,6 +84,6 @@ class IngredientsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def ingredient_params
-      params.permit(:name, :description, :image, :id,)
+      params.require(:ingredient).permit(:name, :description, :image, :id,)
     end
 end
